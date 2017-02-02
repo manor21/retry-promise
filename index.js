708bced3bf6854ce6e95da477743162703dad120
Manor <manor.lahagani@zooz.com>
2017-02-02 10:39:39 +0200

    .

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const retry = (opts = {}, getPromise) => {
-  if (typeof opts === 'function') {
-    return retry({}, opts);
-  }
+'use strict';
 
-  opts.max = opts.max || 10;
-  opts.backoff = opts.backoff || 1000;
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var retry = function retry() {
+    var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var getPromise = arguments[1];
 
-  return new Promise((resolve, reject) => {
-    const attempt = (i) => {
-      getPromise(i)
-        .then(resolve)
-        .catch((err) => {
-          if (i >= opts.max) {
-            return reject(err);
-          }
-          setTimeout(() => attempt(i + 1), opts.increaseInterval ? i * opts.backoff : opts.backoff);
-        });
-    };
-    attempt(1);
-  });
+    if (typeof opts === 'function') {
+        return retry({}, opts);
+    }
+
+    opts.max = opts.max || 10;
+    opts.backoff = opts.backoff || 1000;
+
+    return new Promise(function (resolve, reject) {
+        var attempt = function attempt(i) {
+            getPromise(i).then(resolve).catch(function (err) {
+                if (i >= opts.max) {
+                    return reject(err);
+                }
+                setTimeout(function () {
+                    return attempt(i + 1);
+                }, opts.increaseInterval ? i * opts.backoff : opts.backoff);
+            });
+        };
+        attempt(1);
+    });
 };
 
-export default retry;
+exports.default = retry;
